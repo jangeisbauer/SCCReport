98bf3424db6d36631cf808bd604a5011a27f36b4
Marco Scheel (GK) <Marco.Scheel@glueckkanja.com>
2020-07-09 17:14:05 +0200

    Allow AzureRM or PS Az to access client secret * Add AzureRM back * Remove requirement to have a subscription id * Add another progress bar to show actions

--- ExportAllSCCInvestigationsGH.ps1
+++ ExportAllSCCInvestigationsGH.ps1
@@ -3,12 +3,17 @@
 # Report All Investigations in Security & Compliance Center and export to CSV
 # 
 # - Uses O365 Management API
-# - Uses Client-Secret in Azure Key Vault (to gain access, you need to add somebody to the access policy)
-# - You need to 'install-module azurerm' upfront
+# - Uses Client-Secret in Azure Key Vault (to gain access, you need to add somebody to the access policy) this will require the use of AzureRM or Az Powershell modules
+#  - https://docs.microsoft.com/en-us/powershell/azure/azurerm/install-azurerm-ps
+#  - or https://docs.microsoft.com/en-us/powershell/azure/install-az-ps
+# - You need to run the latest powershell (core) because Invoke-RestRequest with ResponseHeader support is needed and was added only in PS 6 or higher
+
 #
 # Version: 0.1 (2020-5-25)
 # Author: @janvonkirchheim
-# Version: 0.2 (2020-7-08)
+# Version: 0.2 (2020-07-08)
+# Author: @marcoscheel
+# Version: 0.3 (2020-07-09)
 # Author: @marcoscheel
 # Credits to: https://github.com/ssugar/Blog/blob/c34608c3165023d35c65f08899c7ef18b711460c/PowerBIUsage/Scripts/Get-PowerBIUsage.ps1
 # #########################################################################################################################################
@@ -16,17 +21,21 @@
 
 #use keyvault or get secret per Read-Host (for debugging)
 $useKeyVault = $true;
-
+$useKeyVaultAzureRM = $true;
 #specify start and and date attention: only 7 days back are possible! as documentent for start and end toime here: https://docs.microsoft.com/en-us/office/office-365-management-api/office-365-management-activity-api-reference#list-available-content 
 $startTime = [System.DateTime]::UtcNow; #start now #TODO
 $endTime = $startTime.AddHours(24 * -1);#move X hour back #TODO
 
-# after az login check with this command for tenant id and subscription id: Get-AzSubscription
+#Tenant ID that will be connected to read audit log data
 $tenantId = "55ccd7c0-7dd0-414c-8fbb-a8469c7dde2d" #TODO
-$subscriptionId = "bbb7594a-30e2-4928-9631-0588321da565" #TODO
-#kevault
-$keyvaultname = "prd-DGKM-mgmntapi-weu-kv" #TODO
-$keyvaultsecretname = "AADClientSecret" #TODO
+
+#kevault - Only needed if $useKeyVault = $true
+$keyvaulttenant = "tenant.onmicrosoft.com" #TODO or your vanity domain
+#subscription id is only need if multiple subscriptions are available
+#Check https://portal.azure.com/#blade/Microsoft_Azure_Billing/SubscriptionsBlade
+$keyvaultsubscriptionId = "" #TODO
+$keyvaultname = "keyvault" #TODO
+$keyvaultsecretname = "key" #TODO
 
 # AAD application id matching the secret to use for auth
 $appId = "bc86c145-3d2f-435a-9293-39832c7ceb59" #TODO
@@ -41,7 +50,18 @@ $mgmtAuditLogRecordType = 64; # AirInvestigation = Automated incident response (
 # get secret from azure key vault 
 # connect to azure
 $clientSecret = "NOTYETSET";
-if ($useKeyVault)
+#region get secret from keyvault (Az or AzureRM) or console
+if ($useKeyVault -and $useKeyVaultAzureRM)
+{
+    if ($keyvaultsubscriptionId -eq ""){
+        Connect-AzureRmAccount -Tenant $keyvaulttenant
+    }
+    else{
+        Connect-AzureRmAccount -Tenant $keyvaulttenant -Subscription $keyvaultsubscriptionId
+    }
+    $clientSecret = (Get-AzureKeyVaultSecret -vaultName $keyvaultname -name $keyvaultsecretname).SecretValueText
+}
+elseif ($useKeyVault -and $useKeyVaultAzureRM -eq $false)
 {
     #check for az modules
     $AzAccountsPsm = Get-Module -Name Az.Accounts -ListAvailable;
@@ -51,19 +71,33 @@ if ($useKeyVault)
         exit;
     }
     else{
-        Import-Module -Name Az.Accounts;
-        Import-Module -Name Az.KeyVault;
+        Import-Module -Name Az.Accounts -ErrorAction SilentlyContinue;
+        Import-Module -Name Az.KeyVault -ErrorAction SilentlyContinue;
     }
-    $isloggedin = Get-AzContext -ListAvailable | Where-Object { $_.Tenant.Id -eq $tenantId -and $_.Subscription.Id -eq $subscriptionId };
+    $isloggedin = $null;
+
+    if ($null -ne $keyvaultsubscriptionId)
+    {
+        $isloggedin = Get-AzContext -ListAvailable | Where-Object { $_.Subscription.Id -eq $keyvaultsubscriptionId };
+    }
     if ($null -eq $isloggedin){
         #use device authentication to reuse previously authenticated sessions
-        Login-AzAccount -UseDeviceAuthentication -TenantId $tenantId -Subscription $subscriptionId ;
+        if ($keyvaultsubscriptionId -eq ""){
+            Login-AzAccount -UseDeviceAuthentication -TenantId $keyvaulttenant;
+        }
+        else{
+            Login-AzAccount -UseDeviceAuthentication -TenantId $keyvaulttenant -Subscription $keyvaultsubscriptionId;
+        }
+    }
+    else{
+        $isloggedin | Set-AzContext;
     }
     $clientSecret = (Get-AzKeyVaultSecret -VaultName $keyvaultname -Name $keyvaultsecretname).SecretValueText;
 }
 else{
     $clientSecret = Read-Host "Enter client secret for app id: $appId";
 }
+#endregion
 # #########################################################################################################################################
 
 $global:currentAccessToken = "NOTYETSET";
@@ -144,7 +178,7 @@ function Get-AuditLogEntries($start, $end) {
     $maxCounter = $Content.length;
     foreach($item in $Content){
         $counter++;
-        Write-Progress -Id 1 -Activity "Get-AuditLogEntries" -Status "$start - $end ($counter of $maxCounter) matched events: $allBlobContentCount" -PercentComplete (100 * $counter / $Content.length)
+        Write-Progress -Id 2 -ParentId 1 -Activity "Get-AuditLogEntries" -Status "$start - $end ($counter of $maxCounter) matched events: $allBlobContentCount" -PercentComplete (100 * $counter / $Content.length)
         
         $blobContent = Get-AuditLogSubscriptionContentBlob -contentUri $($item.contentUri)
         $allBlobContent += $blobContent
@@ -168,7 +202,7 @@ else{
 #List available content states that one should only return content from max 24 hours
 #https://docs.microsoft.com/en-us/office/office-365-management-api/office-365-management-activity-api-reference#list-available-content
 #To report more work in 24 hour chunks
-write-host "Retrieving log entries created between $startTime and $endTime (if you have choosen too many days to report, go, grab a coffee)"
+write-host ("Retrieving log entries created between " + $startTime.ToString("yyyy-MM-dd HH:mm") + " and " + $endTime.ToString("yyyy-MM-dd HH:mm") +" (if you have choosen too many days to report, go, grab a coffee)")
 
 $currentEnd = $startTime.AddHours($workIntervall * -1);
 $currentStart = $startTime;
@@ -177,6 +211,7 @@ while ($currentStart -gt $endTime) {
     if ($currentEnd -lt $endTime){
         $currentEnd = $endTime; # if current end (due to intervall) is smaller then desired end, set the desired end to not overquery ;)
     }
+    Write-Progress -Id 1 -Activity "Loop through time" -Status ($currentEnd.ToString("yyyy-MM-dd HH:mm") + " - " +  $currentStart.ToString("yyyy-MM-dd HH:mm") ) -PercentComplete -1
     $Entries += Get-AuditLogEntries -start $currentStart -end $currentEnd # this will already be filtered to the record type needed
     $currentStart = $currentEnd;
     $currentEnd = $currentStart.AddHours($workIntervall * -1);
